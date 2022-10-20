234b2e3a28f542c1a91129a623ae22df44ab13f3
$ipt <ray.sh2005@hotmail.co.uk>
2022-10-20 10:41:01 +0100

    newly committed

--- game/index.js
+++ game/index.js
@@ -1,34 +1,249 @@
 const canvas = document.querySelector("canvas")
 const c = canvas.getContext("2d")
 
+// canvas settings
 canvas.width = 1024
 canvas.height = 576
 
-c.fillRect(0,0, canvas.width, canvas.height)
+const gravity = 0.7
 
-class Sprite {
-  constructor(position){
-    this.position = position
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+// creating a background
+const background = new Sprite({
+position: {
+  x: 0,
+  y: 0
+},
+imageSrc: './img/background.png'
+})
+
+// creating a shop in the background
+const shop = new Sprite({
+  position: {
+    x: 470,
+    y: 0
+  },
+  imageSrc: "./img/shop.png",
+  scale: 2.75,
+  framesMax: 6
+})
+
+
+// creating a player and enemy
+
+const player = new Fighter({
+  position: {
+    x: 0,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 10
+  },
+  offset: {
+    x: 0,
+    y: 0
+  }
+})
+
+
+const enemy = new Fighter({
+  position: {
+    x: 400,
+    y: 100
+  },
+  velocity: {
+    x: 0,
+    y: 10
+  },
+  color: 'blue',
+  offset: {
+    x: -50,
+    y: 0
   }
 
-  draw(){
-    c.fillStyle = "red"
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+})
+
+
+decreaseTimer()
+
+console.log(player)
+console.log(enemy)
+console.log("\n")
+
+const keys = {
+  //player keys
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  },
+  //enemy keys
+  ArrowUp: {
+    pressed: false
+  },
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
   }
 }
 
-const player = new Sprite({
-  x: 0,
-  y: 0
+
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = "black"
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  background.update()
+  shop.update()
+  player.update()
+  enemy.update()
+  player.velocity.x = 0
+  enemy.velocity.x = 0
+
+  //player movement
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5
+  }
+  //enemy movement
+  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x = -5
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x = 5
+  }
+
+  //detect for collisions
+  if (rectangularCollision({
+      rectangle1: player,
+      rectangle2: enemy
+    }) &&
+    player.isAttacking) {
+      player.isAttacking = false
+      enemy.health -= 20
+      document.querySelector("#enemyHealth").style.width = enemy.health + "%"
+    console.log("player attacked")
+  }
+
+  if (rectangularCollision({
+      rectangle1: enemy,
+      rectangle2: player
+    }) &&
+    enemy.isAttacking) {
+      enemy.isAttacking = false
+      player.health -= 20
+      document.querySelector("#playerHealth").style.width = player.health + "%"
+
+    console.log("enemy attacked")
+  }
+
+  if(enemy.health <= 0 || player.health <= 0){
+    determineWinner({player, enemy, timerID})
+  }
+}
+animate()
+
+
+
+window.addEventListener("keydown", (event) => {
+  //player keys
+  switch (event.key) {
+    case 'd':
+      player.lastKey = 'd'
+      keys.d.pressed = true
+
+      break;
+
+    case 'a':
+      player.lastKey = 'a'
+      keys.a.pressed = true
+      break;
+
+    case 'w':
+      player.velocity.y = -20
+
+      break;
+
+    case ' ':
+      player.attack()
+      break;
+
+    case 'ArrowDown':
+      enemy.attack()
+      break;
+
+
+  }
+
+  //enemy keys
+  switch (event.key) {
+    case 'ArrowRight':
+      enemy.lastKey = 'ArrowRight'
+      keys.ArrowRight.pressed = true
+
+
+      break;
+    case 'ArrowLeft':
+      enemy.lastKey = 'ArrowLeft'
+      keys.ArrowLeft.pressed = true
+
+
+      break;
+    case 'ArrowUp':
+      enemy.velocity.y = -20
+
+      break;
+  }
+
 })
+window.addEventListener("keyup", (event) => {
+  //player keys
+  switch (event.key) {
+    case 'd':
 
-player.draw()
+      keys.d.pressed = false
 
-const enemy = new Sprite({
-  x: 400,
-  y: 100,
+      break;
+
+    case 'a':
+      keys.a.pressed = false
+
+      break;
+
+    case 'w':
+      keys.w.pressed = false
+
+      break;
+
+    case 'b':
+      keys.b.pressed = false
+      break;
+
+  }
+  //enemy keys
+  switch (event.key) {
+    case 'ArrowRight':
+
+      keys.ArrowRight.pressed = false
+
+      break;
+
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+
+      break;
+
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = false
+
+
+  }
 })
-
-enemy.draw()
-
-console.log(player)
